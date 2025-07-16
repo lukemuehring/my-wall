0cf97b2df8563bb3b297fed0a79ffa36e242020d
Luke Muehring <muehring.luke@gmail.com>
2025-07-16 11:24:05 -0600

    note defaults to initial position from sanity

--- dashboard/src/components/NoteCard.tsx
+++ dashboard/src/components/NoteCard.tsx
@@ -5,8 +5,13 @@ import { motion } from "framer-motion";
 function NoteCard({ note }: { note: INote }) {
   return (
     <motion.div
+      key={note._id}
       drag
       dragMomentum={false}
+      style={{
+        x: note.position?.x ?? 0,
+        y: note.position?.y ?? 0,
+      }}
       className="w-48 h-48 bg-yellow-100 text-black rounded p-4 shadow"
     >
       <h3 className="text-xl font-semibold">{note.title}</h3>
